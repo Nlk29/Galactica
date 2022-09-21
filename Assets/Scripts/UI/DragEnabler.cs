00f4db96eefea035763ff55e57224dd64ebfcef4
Nlk29 <78368286+NikTech06@users.noreply.github.com>
2022-09-21 18:30:04 +0200

    1829210922
    The UI Update!

---
+++ Assets/Scripts/UI/DragEnabler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragEnabler : MonoBehaviour, IDragHandler
+{
+	[SerializeField]
+	private RectTransform dragRectTransform;
+
+	private Canvas canvas;
+
+	void Start()
+	{
+		GameObject temp = GameObject.FindGameObjectWithTag("Canvas");
+
+		canvas = temp.GetComponent<Canvas>();
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+	}
+}
---
+++ Assets/Scripts/UI/DragWindow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragWindow : MonoBehaviour, IDragHandler
+{
+	[SerializeField]
+	private RectTransform dragRectTransform;
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		
+	}
+}
